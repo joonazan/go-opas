8d8333fad9aad80ee901d6b5df456625e330a22f
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2015-10-26 15:11:51 +0200

    toimiva esimerkki viivojen piirtämisestä

--- esimerkit/viivat/main.go
+++ esimerkit/viivat/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"github.com/go-gl/v2.1/gl"
+	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/joonazan/closedgl"
 )
 
 func main() {
-	closedgl.Run(func(dt float64) {
-		gl.Begin(gl.LINES)
-		gl.End()
-	})
+	closedgl.Run(draw, 640, 640, "Kaksi viivaa")
+}
+
+func draw(dt float64) {
+
+	gl.Begin(gl.LINES)
+
+	gl.Color3f(1, 1, 1)
+	gl.Vertex2d(0, 0)
+	gl.Vertex2d(0.5, 0)
+
+	gl.Color3f(1, 0, 0)
+	gl.Vertex2d(0, 0)
+	gl.Vertex2d(0, 0.5)
+
+	gl.End()
 }
