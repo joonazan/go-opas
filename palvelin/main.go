ed22d09a48d2db4d148b620268ba99e0a50ef821
Joonatan Saarhelo <joon.saar@gmail.com>
2016-01-05 18:14:48 +0200

    tietorakenne edistymisen seuraamiseksi

--- palvelin/main.go
+++ palvelin/main.go
@@ -10,10 +10,34 @@ const PalvelimenOsoite = "http://localhost:8080"
 func main() {
 
 	http.HandleFunc("/", loginRequired(func(w http.ResponseWriter, r *http.Request, u User) {
-		fmt.Fprintf(w, "%#v", u)
+		fmt.Fprint(w, edistymiset.Edistyminen(u.Email))
 	}))
 
 	http.HandleFunc("/authcallback/google", authentication)
 
 	panic(http.ListenAndServe(":8080", nil))
 }
+
+const (
+	EiTehty = iota
+	Tehty
+	Aloitettu
+)
+
+type Edistyminen map[string]uint8
+
+type Edistymiset map[string]Edistyminen
+
+func (e Edistymiset) Edistyminen(oppilas string) Edistyminen {
+	if o, ok := e[oppilas]; ok {
+		return o
+	}
+	e[oppilas] = make(Edistyminen)
+	return e[oppilas]
+}
+
+func (e Edistymiset) Tallenna() {
+
+}
+
+var edistymiset = make(Edistymiset)
