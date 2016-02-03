e7f6e8663c3a9281cdc44c24eb901c0557f88811
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2016-02-03 17:02:56 +0200

    lisätty edistymisten yhteenveto

--- palvelin/progressdump.go
+++ palvelin/progressdump.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/progressdump", loginRequired(func(w http.ResponseWriter, r *http.Request, u User) {
-		if u.Email == "joon.saar@gmail.com" {
-			text, err := json.Marshal(edistymiset)
-			if err != nil {
-				w.Write([]byte(err.Error()))
+	adminpage := func(path string, page func(http.ResponseWriter)) {
+		http.HandleFunc(path, loginRequired(func(w http.ResponseWriter, r *http.Request, u User) {
+			if u.Email == "joon.saar@gmail.com" {
+				page(w)
 			}
-			w.Write(text)
+		}))
+	}
+
+	adminpage("/progressdump", func(w http.ResponseWriter) {
+		text, err := json.Marshal(edistymiset)
+		if err != nil {
+			w.Write([]byte(err.Error()))
 		}
-	}))
+		w.Write(text)
+	})
+
+	adminpage("/progresssummary", func(w http.ResponseWriter) {
+
+		edistymiset.RLock()
+		defer edistymiset.RUnlock()
+
+		for email, edistyminen := range edistymiset.data {
+
+			tilalaskuri := make([]int, TilojenMäärä)
+
+			edistyminen.RLock()
+			for _, tila := range edistyminen.data {
+				tilalaskuri[tila]++
+			}
+			edistyminen.RUnlock()
+
+			line := fmt.Sprintf("%s tehty: %d aloitettu: %d", email, tilalaskuri[Tehty], tilalaskuri[Aloitettu])
+			w.Write([]byte(line))
+		}
+	})
 }
