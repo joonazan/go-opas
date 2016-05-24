956673f4d44768165a0116d5f860a7c6f0bd8e0b
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2016-05-24 17:20:06 +0300

    koodin ajaminen serverillä mahdollista, timeout

--- palvelin/koe.go
+++ palvelin/koe.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	"github.com/zond/gosafe"
+	"html/template"
 	"net/http"
 )
 
 func init() {
 
-	compiler := gosafe.NewCompiler()
-	for _, allowed := range []string{"fmt", "math"} {
-		compiler.Allow(allowed)
+	t, err := template.ParseFiles("data/koe.html")
+	if err != nil {
+		panic(err)
 	}
 
-	loginRequired("/koe", func(w http.ResponseWriter, r *http.Request, u User) {})
+	const (
+		koeURL    = "/koe"
+		codeField = "koodi"
+	)
+
+	loginRequired(koeURL, func(w http.ResponseWriter, r *http.Request, u User) {
+		r.ParseForm()
+
+		var reply, code string
+		if len(r.Form) != 0 {
+			code = r.FormValue(codeField)
+			output, err := Run(code)
+			if err != nil {
+				reply = err.Error()
+			} else {
+				reply = output
+			}
+		}
+
+		t.Execute(w, struct {
+			FormAction, CodeName, Code, Reply string
+		}{FormAction: koeURL,
+			CodeName: codeField,
+			Reply:    reply,
+			Code:     code,
+		})
+	})
 }
