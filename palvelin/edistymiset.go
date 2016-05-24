36f15713f6ddbbdfc755784a2048eb4c115899b2
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2016-05-24 20:46:56 +0300

    conditional compilation tietokanta vs tiedostojärjestelmä -valintaan

--- palvelin/edistymiset.go
+++ palvelin/edistymiset.go
@@ -23,26 +23,6 @@ var (
 	oldfile    = path.Join(savefolder, "vanhat_edistymiset.json")
 )
 
-func lataaEdistymiset() Edistymiset {
-
-	e := Edistymiset{}
-	bytes, err := ioutil.ReadFile(savefile)
-	if err == nil {
-		err := json.Unmarshal(bytes, &e.data)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("Tila ladattu levyltä!")
-	} else if os.IsNotExist(err) {
-		log.Println("Tallennustiedostoa ei löytynyt. Aloitan tyhjästä.")
-		e.data = make(map[string]Edistyminen)
-	} else {
-		log.Fatal(err)
-	}
-
-	return e
-}
-
 type Edistymiset struct {
 	sync.RWMutex
 	data map[string]Edistyminen
@@ -83,31 +63,6 @@ func (e Edistymiset) TallennaVälein(kesto time.Duration) {
 			}
 		}
 	}()
-}
-
-func (e Edistymiset) Tallenna() error {
-	e.RLock()
-	data, err := json.Marshal(e)
-	if err != nil {
-		return err
-	}
-	e.RUnlock()
-
-	err = os.MkdirAll(savefolder, os.ModePerm)
-	if err != nil {
-		return err
-	}
-	err = os.Rename(savefile, oldfile)
-	if err != nil && !os.IsNotExist(err) {
-		return err
-	}
-	err = ioutil.WriteFile(savefile, data, os.ModePerm)
-	if err != nil {
-		os.Rename(oldfile, savefile)
-		return err
-	}
-
-	return nil
 }
 
 func (e Edistymiset) MarshalJSON() ([]byte, error) {
