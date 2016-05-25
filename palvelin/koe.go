9c555f936449d9e1a95830256faa4da1e877f9c0
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2016-05-25 19:24:17 +0300

    edellinen koodi näytetään

--- palvelin/koe.go
+++ palvelin/koe.go
@@ -65,6 +65,15 @@ func init() {
 		tehtävät = append(tehtävät, tehtävä)
 	}
 
+	previousSolution := func(u User, kokeenNimi string) string {
+		tehtävä := kokeenVaihe(u, kokeenNimi)
+		if oikein(u, kokeenNimi, tehtävä) {
+			return koodi(u, kokeenNimi, tehtävä)
+		} else {
+			return tehtävät[tehtävä].mallikoodi
+		}
+	}
+
 	loginRequired(koeURL, func(w http.ResponseWriter, r *http.Request, u User) {
 
 		vaihe := kokeenVaihe(u, kokeenNimi)
@@ -76,19 +85,20 @@ func init() {
 			var correct bool
 			correct, reply = grade(code, syötteet, tehtävä.tulosteet)
 			if correct {
-				edistyKokeessa(u, kokeenNimi)
+				edistyKokeessa(u, kokeenNimi, true)
 			}
 		}
 
 		t.Execute(w, struct {
-			FormAction, CodeName, Code, Reply string
-			Description                       template.HTML
+			FormAction, CodeName, Code, Reply, PreviousSolution string
+			Description                                         template.HTML
 		}{
-			FormAction:  koeURL,
-			CodeName:    codeField,
-			Description: tehtävä.kuvaus,
-			Code:        code,
-			Reply:       reply,
+			FormAction:       koeURL,
+			CodeName:         codeField,
+			Description:      tehtävä.kuvaus,
+			Code:             code,
+			Reply:            reply,
+			PreviousSolution: previousSolution(u, kokeenNimi),
 		})
 	})
 }
@@ -105,12 +115,33 @@ func kokeenVaihe(u User, kokeenNimi string) int64 {
 	return lueLuku(kokelasAvain(u, kokeenNimi))
 }
 
-func edistyKokeessa(u User, kokeenNimi string) {
-	redisClient.Set(kokelasAvain(u, kokeenNimi), kokeenVaihe(u, kokeenNimi)+1, 0)
+func edistyKokeessa(u User, kokeenNimi string, oikein bool) {
+	avain := kokelasAvain(u, kokeenNimi)
+	vaihe := kokeenVaihe(u, kokeenNimi)
+	redisClient.Set(avain, vaihe+1, 0)
+	var asInt int64
+	if oikein {
+		asInt = 1
+	} else {
+		asInt = 0
+	}
+	redisClient.Set(oikeinAvain(avain, vaihe), asInt, 0)
+}
+
+func oikein(u User, kokeenNimi string, vaihe int64) bool {
+	i, _ := redisClient.Get(oikeinAvain(kokelasAvain(u, kokeenNimi), vaihe)).Int64()
+	if i == 1 {
+		return true
+	}
+	return false
 }
 
 func kokelasAvain(u User, kokeenNimi string) string {
 	return u.Email + kokeenNimi
+}
+
+func oikeinAvain(x string, y int64) string {
+	return fmt.Sprintf("OV%d", y) + x
 }
 
 func lueLuku(key string) int64 {
@@ -119,6 +150,14 @@ func lueLuku(key string) int64 {
 		return 0
 	}
 	return resp
+}
+
+func koodi(u User, kokeenNimi string, tehtävä int64) string {
+	return redisClient.Get(tehtäväAvain(u, kokeenNimi, tehtävä)).String()
+}
+
+func tehtäväAvain(u User, kokeenNimi string, tehtävä int64) string {
+	return fmt.Sprintf("%d", tehtävä) + kokelasAvain(u, kokeenNimi)
 }
 
 func grade(code string, inputs, outputs []string) (bool, string) {
